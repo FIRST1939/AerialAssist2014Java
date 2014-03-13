b2d8f19f7bf2c5c7715bc83cf7888cd1db8b0e62
Joey Holliday <joeyholliday77@gmail.com>
2014-03-13 10:52:27 -0500

    Decrease electric solenoid delay

--- AerialAssist2014/src/org/usfirst/frc1939/AerialAssist2014/commands/KickLatch.java
+++ AerialAssist2014/src/org/usfirst/frc1939/AerialAssist2014/commands/KickLatch.java
@@ -30,7 +30,7 @@ public class  KickLatch extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        this.setTimeout(0.5);
+        this.setTimeout(0.3);
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
@@ -44,7 +44,7 @@ public class  KickLatch extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
-            RobotMap.catapultElectricSolenoid.set(Relay.Value.kOff);
+        RobotMap.catapultElectricSolenoid.set(Relay.Value.kOff);
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
