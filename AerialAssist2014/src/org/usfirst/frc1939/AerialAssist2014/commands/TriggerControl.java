e95a858e9afdd685f0b44d216444b688fafda565
Joey Holliday <joeyholliday77@gmail.com>
2014-03-12 22:05:35 -0500

    Initialize "ready" variable before reading

--- AerialAssist2014/src/org/usfirst/frc1939/AerialAssist2014/commands/TriggerControl.java
+++ AerialAssist2014/src/org/usfirst/frc1939/AerialAssist2014/commands/TriggerControl.java
@@ -26,6 +26,7 @@ public class  TriggerControl extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
+        ready = false;
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
